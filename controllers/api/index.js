722673fd5792f14a4d3eeb33d2cf664024b8a0c3
jochun <jkchun@protonmail.com>
2022-03-29 10:15:39 -1000

    update api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
 
-router.use('/post', postRoutes);
-router.use('/user', userRoutes);
-router.use('/comment', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
