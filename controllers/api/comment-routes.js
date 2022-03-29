4596e739bfebac244ac0748d199ce6aa85fb0386
jochun <jkchun@protonmail.com>
2022-03-29 09:50:08 -1000

    update comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,5 @@
 const { Comment } = require('../../models');
 const router = require('express').Router();
-const withAuth = require('../../utils/auth');
 
 
 router.get('/', (req, res) => {
@@ -12,69 +11,31 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/:id', (req, res) => {
-    Comment.findAll({
-        where: {
-            id: req.params.id
-        }
+router.post('/', (req, res) => {
+    Comment.create({
+        comment: req.body.comment_body,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
     })
-        .then(commentData => res.json(commentData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => res.status(500).json(err));
 });
 
-router.post('/', withAuth, (req, res) => {
-    if (req.session) {
-        Comment.create({
-            comment_text: req.body.comment_text,
-            post_id: req.body.post_id,
-            user_id: req.session.user_id,
-        })
-            .then(commentData => res.json(commentData))
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            })
-    }
-});
-
-
-router.put('/:id', withAuth, (req, res) => {
-    Comment.update({
-        comment_text: req.body.comment_text
-    }, {
-        where: {
-            id: req.params.id
-        }
-    }).then(commentData => {
-        if (!commentData) {
-            res.status(404).json({ message: 'There is no comment found with this id' });
-            return;
-        }
-        res.json(commentData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.delete('/:id', withAuth, (req, res) => {
+// DELETE /api/comments/1
+router.put('/:id', (req, res) => {
     Comment.destroy({
         where: {
             id: req.params.id
         }
-    }).then(commentData => {
-        if (!commentData) {
-            res.status(404).json({ message: 'There is no comment found with this id' });
-            return;
-        }
-        res.json(commentData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
